af054bc394568798e086cb015aa83f2b3695edf1
John An <junghooonan@gmail.com>
2023-10-19 21:04:08 -0700

    added ReverseVowels.js

---
+++ leetcode/array/ReverseVowels.js
@@ -0,0 +1,37 @@
+// Given a string s, reverse only all the vowels in the string and return it.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    // define vowels
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    // split s into an array of letters
+    let letters = s.split('');
+    // initialize left, right
+    let left = 0;
+    let right = s.length;
+    // while left < right
+    while (left < right) {
+        // if letters[left] is a vowel
+        if (vowels.includes(letters[left])) {
+            // if letters[right] is a vowel
+            if (vowels.includes(letters[right])) {
+                // swap letters
+                [letters[left], letters[right]] = [letters[right], letters[left]];
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+        } else {
+            left++;
+        }
+    }
+    // return letters.join('')
+    return letters.join('');
+};
+
+console.log(reverseVowels("hello"));
+console.log(reverseVowels("leetcode"));
